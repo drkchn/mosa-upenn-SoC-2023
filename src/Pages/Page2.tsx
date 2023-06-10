47817cc2ea858a0b9f9226f9650d96169c25d69b
Connor Fech <cfech44@gmail.com>
2023-06-10 01:55:50 -0400

    #13 - adding in react router, a nav bar and a couple different example pages [cf]

---
+++ src/Pages/Page2.tsx
@@ -0,0 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
+export function Page2() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "600px",
+        backgroundColor: "green",
+      }}
+    >
+      <Typography>Page 2</Typography>
+    </Box>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar variant="regular" sx={{ justifyContent: "space-between" }}>
+        <Typography variant="h6" color="inherit" component="div">
+          Basic Tool bar
+        </Typography>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/page2"}>page2</Link>
+      </Toolbar>
+    </AppBar>
+  );
+}
