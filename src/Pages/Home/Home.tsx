b0c8d9fbe6a427a38733657411db2f8515c8e4f6
drkchn <57195399+drkchn@users.noreply.github.com>
2023-06-19 19:38:47 -0700

    [#9] Working text box [DC]

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,31 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+
+const MyComponent: React.FC = () => {
+  const [value, setValue] = useState<string>('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+  return (
+    <TextField
+      label='Enter your residential address'
+      value={value}
+      onChange={handleChange}
+      placeholder='Enter your residential address'
+      fullWidth
+    />
+  );
+};
+
 export function Home() {
   return (
     <div>
       <h1>Welcome to Civics Central!</h1>
       <MyComponent />
     </div>
-  )
+  );
 }
