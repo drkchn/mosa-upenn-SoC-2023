2c05ce6cd323c12df3d374e99abac265f5dcc018
drkchn <57195399+drkchn@users.noreply.github.com>
2023-06-19 19:42:14 -0700

    [#9] Formatting code [DC]

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
 
 const MyComponent: React.FC = () => {
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
   return (
     <TextField
-      label='Enter your residential address'
+      label="Enter your residential address"
       value={value}
       onChange={handleChange}
-      placeholder='Enter your residential address'
+      placeholder="Enter your residential address"
       fullWidth
     />
   );
