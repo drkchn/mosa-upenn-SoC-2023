8154d9840cc7b6c4b8998aea7331700667539b4a
drkchn <57195399+drkchn@users.noreply.github.com>
2023-06-24 21:20:33 -0700

    [#29] Added icon and grouped name together [DC]

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -4,9 +4,12 @@ import {
   PaletteMode,
   Toolbar,
   Typography,
+  IconButton,
+  Box,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { ThemeToggle } from "../themeToggle/ThemeToggle.tsx";
+import HowToVoteIcon from "@mui/icons-material/HowToVote";
 
 interface navBarProps {
   mode: PaletteMode;
@@ -23,9 +26,20 @@ export function Navbar({ mode, setMode }: navBarProps) {
           flexDirection: { xs: "column", sm: "row" },
         }}
       >
-        <Typography variant="h6" color="inherit" component="h1">
-          Civics Tool
-        </Typography>
+        <Box display="flex" alignItems="center">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            component={Link} to={"/"}
+          >
+            <HowToVoteIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="h1">
+            Civics Central
+          </Typography>
+        </Box>
         <Button component={Link} to={"/"} color="inherit">
           Home
         </Button>
